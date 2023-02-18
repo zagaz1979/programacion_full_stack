e0306fa2d84e1682ccf3959513a81d4a074fa107
Cesar <cesar.garcia.suarez@gmail.com>
2023-02-17 20:31:39 -0500

    Creando script en package.json

---
+++ curso-javascript/mi-primer-proyecto-node/index.js
@@ -0,0 +1,11 @@
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+//Este es el mensaje de despedida
+console.log("Adios");
+
+/* 
+Comentarios multilinea
+una linea
+dos lineas
+*/
