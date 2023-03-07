b20f33f62b748bcca0f8b143543cd2097af050fc
Cesar Garcia <cesar.garcia.suarez@gmail.com>
2023-03-06 20:06:46 -0500

    Numbers y presicion

---
+++ curso-javascript/05_numbers/numeros_y_presicion.js
@@ -0,0 +1,17 @@
+// Sesio 5 numbers
+
+// Declaracion de variables numericas (enteros y con decimales)
+let a = 5;
+console.log(a);
+
+let b = 0.1;
+console.log(b);
+
+let c = 0.2;
+
+console.log(b + c);
+
+console.log(b + c);
+
+// Truco para resiolver esta operacion
+console.log(Math.round( (0.1 + 0.2) * 100) / 100)
